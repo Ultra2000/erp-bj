cac6f22caeda57c1fe85e14de6a5d2ac7ce7d921
User2000 <frejusbouraima79@gmail.com>
2026-02-01 00:06:39 +0100

    Fix: RestrictedForCashier respecte shouldRegisterNavigation=false des ressources

--- app/Filament/Traits/RestrictedForCashier.php
+++ app/Filament/Traits/RestrictedForCashier.php
@@ -19,9 +19,21 @@ trait RestrictedForCashier
 
     /**
      * Cacher la navigation pour les caissiers
+     * Respecte aussi $shouldRegisterNavigation = false si défini dans la classe
      */
     public static function shouldRegisterNavigation(): bool
     {
+        // Respecter la propriété de la classe si elle est définie à false
+        if (property_exists(static::class, 'shouldRegisterNavigation')) {
+            $reflection = new \ReflectionProperty(static::class, 'shouldRegisterNavigation');
+            if ($reflection->getDeclaringClass()->getName() === static::class) {
+                $value = $reflection->getValue();
+                if ($value === false) {
+                    return false;
+                }
+            }
+        }
+        
         return !static::isCashierUser();
     }
 
