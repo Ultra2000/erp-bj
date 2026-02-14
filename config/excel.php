6319ffee242fff13da61c83d55992d6c92f6c615
User2000 <frejusbouraima79@gmail.com>
2026-02-14 01:06:43 +0100

    Fix config:cache - remplacer toutes les constantes de classe par des strings

--- config/excel.php
+++ config/excel.php
@@ -1,7 +1,5 @@
 <?php
 
-use Maatwebsite\Excel\Excel;
-
 return [
     'exports' => [
 
@@ -177,21 +175,21 @@ return [
     |
     */
     'extension_detector' => [
-        'xlsx'     => Excel::XLSX,
-        'xlsm'     => Excel::XLSX,
-        'xltx'     => Excel::XLSX,
-        'xltm'     => Excel::XLSX,
-        'xls'      => Excel::XLS,
-        'xlt'      => Excel::XLS,
-        'ods'      => Excel::ODS,
-        'ots'      => Excel::ODS,
-        'slk'      => Excel::SLK,
-        'xml'      => Excel::XML,
-        'gnumeric' => Excel::GNUMERIC,
-        'htm'      => Excel::HTML,
-        'html'     => Excel::HTML,
-        'csv'      => Excel::CSV,
-        'tsv'      => Excel::TSV,
+        'xlsx'     => 'Xlsx',
+        'xlsm'     => 'Xlsx',
+        'xltx'     => 'Xlsx',
+        'xltm'     => 'Xlsx',
+        'xls'      => 'Xls',
+        'xlt'      => 'Xls',
+        'ods'      => 'Ods',
+        'ots'      => 'Ods',
+        'slk'      => 'Slk',
+        'xml'      => 'Xml',
+        'gnumeric' => 'Gnumeric',
+        'htm'      => 'Html',
+        'html'     => 'Html',
+        'csv'      => 'Csv',
+        'tsv'      => 'Csv',
 
         /*
         |--------------------------------------------------------------------------
@@ -202,7 +200,7 @@ return [
         | Available options: Excel::MPDF | Excel::TCPDF | Excel::DOMPDF
         |
         */
-        'pdf'      => Excel::DOMPDF,
+        'pdf'      => 'Dompdf',
     ],
 
     /*
@@ -223,7 +221,7 @@ return [
     |
     */
     'value_binder'       => [
-        'default' => Maatwebsite\Excel\DefaultValueBinder::class,
+        'default' => 'Maatwebsite\\Excel\\DefaultValueBinder',
     ],
 
     'cache'        => [
